0c0ada5c220e9dd802ba14e63758715e2a8f125c
Manish Choudhary <manishchoudharyy7@gmail.com>
2026-02-28 12:18:54 +0530

    Add Landing page as main application entry point

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/landing/Navbar'
+import Hero from '../components/landing/Hero'
+import HowItWorks from '../components/landing/HowItWorks'
+import Features from '../components/landing/Features'
+import CTA from '../components/landing/CTA'
+import Footer from '../components/landing/Footer'
+
+const Landing = () => {
+  return (
+    <div className='min-h-screen w-full'>
+      <Navbar />
+      <Hero />
+      <HowItWorks />
+      <Features />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
+
+export default Landing
